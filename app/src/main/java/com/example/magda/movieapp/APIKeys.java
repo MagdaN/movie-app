de72fb9a0c5b995c8096dc3e8a86073f0d842131
magda <magdalena.noffke@googlemail.com>
2017-06-05 20:54:51 +0200

    add APIKeys class to git

---
+++ app/src/main/java/com/example/magda/movieapp/APIKeys.java
@@ -0,0 +1,10 @@
+package com.example.magda.movieapp;
+
+public class APIKeys {
+
+    private static final String mMovieDBAPIKey = "";
+
+    public static String getmMovieDBAPIKey() {
+        return mMovieDBAPIKey;
+    }
+}
